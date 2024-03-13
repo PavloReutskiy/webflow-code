a3fbceca0eaebe87e4dbf9f77e9769913bb7f924
PavloReutskiy <87998640+PavloReutskiy@users.noreply.github.com>
2024-03-13 14:29:29 +0200

    added test code for title4

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
   const element = document.querySelector('.heading-style-h2');
-  if (element) {
-    element.style.color = '#007aff !important';
-  }
+  element.style.color = '#007aff !important';
 });
