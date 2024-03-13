172ef776b0521bd6cce7cb361933b5b0cc9f0b32
PavloReutskiy <87998640+PavloReutskiy@users.noreply.github.com>
2024-03-13 14:12:13 +0200

    added test code for title1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const element = document.querySelector('.heading-style-h1');
+  const element = document.querySelector('.heading-style-h2');
   if (element) {
     element.style.color = 'green';
   }
