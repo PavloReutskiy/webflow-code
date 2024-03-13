91caf300d43c2205a9fd12047fb37e9c14f7883a
PavloReutskiy <87998640+PavloReutskiy@users.noreply.github.com>
2024-03-13 13:50:56 +0200

    added test code

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const element = document.querySelector('.clients-title');
+  if (element) {
+    element.style.color = 'green';
+  }
+});
