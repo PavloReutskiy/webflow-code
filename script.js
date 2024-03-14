6eb58ac664c18a3f2d50b6bb643d84e2c50349c2
PavloReutskiy <87998640+PavloReutskiy@users.noreply.github.com>
2024-03-14 09:54:31 +0200

    added test code for title8

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
   const element = document.querySelector('.hero-title');
-  element.style.color = '#007aff';
+  element.style.color = '#02451f';
 });
